a2d72266842a8d247a5418b246466e22db090590
majuuzera <majuthelink@gmail.com>
2022-02-16 19:06:36 -0300

    1 commit

---
+++ aula01/controle/function.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    // Monitorar o clique em cima do botão btn-send
+    $('.btn-send').click(function(e) {
+        e.preventDefault()
+
+        alert('Você clicou no botão enviar!!')
+    })
+
+})
---
+++ aula01/modelo/retorno.php
@@ -0,0 +1,35 @@
+<?php
+
+    $nome = $_REQUEST['nome'];
+    $animal = $_REQUEST['animal'];
+
+    if(empty($nome)){
+        
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'There are unfilled fields.'
+        );
+    
+    } else {
+
+        switch($animal){
+            case '1' : $dados = array(
+                "tipo" => 'pincher.jfif',
+                "mensagem" => 'Hello'.$nome.', we know that your favorite animal is a dog'
+            );
+            break;
+            case '2' : $dados = array(
+                "tipo" => 'picapau.jfif',
+                "mensagem" => 'Hello '.$nome.', we know that your favorite animal is a bird'
+            );
+            break;
+            case '3' : $dados = array(
+                "tipo" => 'mbappe.jfif',
+                "mensagem" => 'Hello '.$nome.', we know that your favorite animal is a turtle'
+            );
+            break;
+        }
+
+    }
+
+    echo json_encode($dados);
